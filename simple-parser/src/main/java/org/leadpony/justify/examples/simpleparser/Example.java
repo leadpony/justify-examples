2b5f916c6d2234d900ffb976b017615a0a4b82bf
leadpony <julyborn715+leadpony@gmail.com>
2018-08-26 01:22:49 +0900

    Update for new API.

--- simple-parser/src/main/java/org/leadpony/justify/examples/simpleparser/Example.java
+++ simple-parser/src/main/java/org/leadpony/justify/examples/simpleparser/Example.java
@@ -1,39 +1,35 @@
 package org.leadpony.justify.examples.simpleparser;
 
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.json.stream.JsonParser;
 
 import org.leadpony.justify.core.JsonSchema;
 import org.leadpony.justify.core.Jsonv;
-import org.leadpony.justify.core.Problem;
+import org.leadpony.justify.core.ProblemHandler;
 
 /**
  * Validating JSON with the Streaming API.
  */
 public class Example {
 
-    private static final Path SCHEMA_PATH = Paths.get("person.schema.json");
-    private static final Path INSTANCE_PATH = Paths.get("person-bad.json");
-    
     public void run() throws IOException {
-        JsonSchema schema = Jsonv.readSchema(Files.newInputStream(SCHEMA_PATH));
+        Jsonv jsonv = Jsonv.newInstance();
         
-        List<Problem> problems = new ArrayList<>();
-        try (JsonParser parser = Jsonv.createParser(
-                Files.newInputStream(INSTANCE_PATH), schema, problems::addAll)) {
+        Path pathToSchema = Paths.get("person.schema.json");
+        JsonSchema schema = jsonv.readSchema(pathToSchema);
+
+        // Problem handler
+        ProblemHandler handler = ProblemHandler.printingWith(System.out::println);
+
+        Path pathToInstance = Paths.get("person-bad.json");
+        try (JsonParser parser = jsonv.createParser(pathToInstance, schema, handler)) {
             while (parser.hasNext()) {
                 JsonParser.Event event = parser.next();
                 System.out.println(event);
             }
-        }
-        if (!problems.isEmpty()) {
-            problems.forEach(System.out::println);
         }
     }
     
--- simple-reader/src/main/java/org/leadpony/justify/examples/simplereader/Example.java
+++ simple-reader/src/main/java/org/leadpony/justify/examples/simplereader/Example.java
@@ -1,38 +1,34 @@
 package org.leadpony.justify.examples.simplereader;
 
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.json.JsonReader;
 import javax.json.JsonValue;
 
 import org.leadpony.justify.core.JsonSchema;
 import org.leadpony.justify.core.Jsonv;
-import org.leadpony.justify.core.Problem;
+import org.leadpony.justify.core.ProblemHandler;
 
 /**
  * Validating JSON with the Object Model API.
  */
 public class Example {
     
-    private static final Path SCHEMA_PATH = Paths.get("person.schema.json");
-    private static final Path INSTANCE_PATH = Paths.get("person-bad.json");
-
     public void run() throws IOException {
-        JsonSchema schema = Jsonv.readSchema(Files.newInputStream(SCHEMA_PATH));
+        Jsonv jsonv = Jsonv.newInstance();
+      
+        Path pathToSchema = Paths.get("person.schema.json");
+        JsonSchema schema = jsonv.readSchema(pathToSchema);
+
+        // Problem handler
+        ProblemHandler handler = ProblemHandler.printingWith(System.out::println);
         
-        List<Problem> problems = new ArrayList<>();
-        try (JsonReader reader = Jsonv.createReader(
-                Files.newInputStream(INSTANCE_PATH), schema, problems::addAll)) {
+        Path pathToInstance = Paths.get("person-bad.json");
+        try (JsonReader reader = jsonv.createReader(pathToInstance, schema, handler)) {
             JsonValue value = reader.readValue();
             System.out.println(value);
-        }
-        if (!problems.isEmpty()) {
-            problems.forEach(System.out::println);
         }
     }
 

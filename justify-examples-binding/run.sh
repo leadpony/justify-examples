032cad84f5b373f055491ecf060f31ca961570c3
leadpony <julyborn715+leadpony@gmail.com>
2018-12-23 21:48:49 +0900

    Fix line ending.

--- justify-examples-binding/run.sh
+++ justify-examples-binding/run.sh
@@ -2,4 +2,4 @@
 JAVA=$JAVA_HOME/bin/java
 "$JAVA" -p target/modules/ \
 -m org.leadpony.justify.examples.binding/org.leadpony.justify.examples.binding.Example \
-person.schema.json person-bad.json 
+person.schema.json person-bad.json
